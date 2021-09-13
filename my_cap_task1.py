6e0a4f4e5e1e126b5622f13e6e8b008cf79e3543
ShyamaliN <71086088+ShyamaliN@users.noreply.github.com>
2021-09-13 20:11:47 +0530

    Create my_cap_task1.py

---
+++ my_cap_task1.py
@@ -0,0 +1,6 @@
+import math
+radius = float(input("Input the radius of the circle is :"))
+print(radius)
+area = math.pi *radius * radius
+print("The area of circle with radius ",radius,"is : ")
+print(area)
